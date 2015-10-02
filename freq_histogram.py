05547910be8b9e23c4dca9273274b7ad0a8db112
Jacob Lichtstrahl <jacob.lichtstrahl@students.makeschool.com>
2015-10-01 23:40:06 -0700

    Add relative_probability method (note: still not random)

--- freq_histogram.py
+++ freq_histogram.py
@@ -14,15 +14,15 @@ def histogram_creator(words_from_user_input):
 
 
 def cumulative_distribution(histogram_creator_return):
-    cum_list = []
+    distribution_list = []
     distribution_range = 0
 
     for word, freq in histogram_creator_return.items():
-        if word not in cum_list:
+        if word not in distribution_list:
             upper_limit = distribution_range + freq
             distribution_range += freq
-            cum_list.append((word, upper_limit))
-    return cum_list
+            distribution_list.append((word, upper_limit))
+    return distribution_list
 
 
 def sample_from_distribution(cumulative_distribution_return):
@@ -41,5 +41,14 @@ def main():
     return sample_from_distribution(cum_distribution_returns)
 
 
+def relative_probability():
+    check_list = []
+    for result in range(10000):
+        sample_from_distribution = main()
+        check_list.append(sample_from_distribution)
+    print(histogram_creator(check_list))
+    return histogram_creator(check_list)
+
+
 if __name__ == '__main__':
-    print(main())
+    relative_probability()
